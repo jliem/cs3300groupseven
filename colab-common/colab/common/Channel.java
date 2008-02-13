2fe1995cf0f7343028aaa57b1a21c3fc1517fa77
JohannesLiem <JohannesLiem@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-13 22:23:53 +0000

    Updated classpath to use /bin folder as output. Added some Channel stuff.

--- colab-common/colab/common/Channel.java
+++ colab-common/colab/common/Channel.java
@@ -1,6 +1,22 @@
 package colab.common;
 
-public class Channel {
+import java.util.ArrayList;
+import java.util.Collection;
 
+/**
+ * Represents a channel in the system, including data, usernames and metadata.
+ * 
+ * @author JL
+ *
+ */
+public abstract class Channel {
 	
+	protected ArrayList<User> userList;
+	
+	public Channel() {
+		userList = new ArrayList<User>();
+	}
+	
+	public abstract void add(ChannelData data);
+	public abstract boolean remove(ChannelData data);
 }
---
+++ colab-common/colab/common/ChannelData.java
@@ -0,0 +1,10 @@
+package colab.common;
+
+import java.util.Date;
+
+public abstract class ChannelData {
+
+	protected User creator;
+	protected Date timestamp;
+	
+}

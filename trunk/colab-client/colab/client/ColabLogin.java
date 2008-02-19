6328982cc14f347ad97259d740e0ade0c00bdc4f
dancepants <dancepants@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-19 16:42:33 +0000

    Not not implemented constructors.

---
+++ trunk/colab-client/colab/client/ColabLogin.java
@@ -0,0 +1,26 @@
+package colab.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+
+public class ColabLogin implements ActionListener{
+
+	public ColabLogin(String username, char[] password,
+			String serverIP) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+
+}

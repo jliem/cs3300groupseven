d7319bab8dca7eaa644a3261b5fb748a6410c614
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-19 18:08:39 +0000

    community login works

--- colab/colab/client/ColabClient.java
+++ colab/colab/client/ColabClient.java
@@ -2,8 +2,9 @@ package colab.client;
 
 import java.rmi.Naming;
 
+import colab.community.CommunityName;
+import colab.server.ColabServerInterface;
 import colab.server.ConnectionInterface;
-import colab.server.ServerInterface;
 import colab.user.UserName;
 
 public class ColabClient {
@@ -23,11 +24,18 @@ public class ColabClient {
 		
         String url = "//localhost:" + port + "/COLAB_SERVER";
         
-		ServerInterface server = (ServerInterface) Naming.lookup(url);
+		ColabServerInterface server = (ColabServerInterface) Naming.lookup(url);
 		ConnectionInterface connection = server.connect();
 		boolean correct = connection.logIn(new UserName("Chris"), "pass4");
 		if (correct) {
 			System.out.println("User logged in.");
+			correct = 
+				connection.logIn(new CommunityName("Team Awesome"), "awesomePass");
+			if (correct) {
+				System.out.println("Logged into community.");
+			} else {
+				System.out.println("Community login failed.");
+			}
 		} else {
 			System.out.println("Login failed.");
 		}
--- colab/colab/community/Community.java
+++ colab/colab/community/Community.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import colab.identity.Identifiable;
+import colab.user.Password;
 import colab.user.User;
 
 public class Community implements Identifiable<CommunityName>, Serializable {
@@ -12,21 +13,24 @@ public class Community implements Identifiable<CommunityName>, Serializable {
 	public static final long serialVersionUID = 1L;
 	
 	private final CommunityName name;
-
 	private final Collection<User> members;
-
-	public Community(final CommunityName name) {
+	private Password password;
+	
+	public Community(final CommunityName name, final Password password) {
 
 		// Set the community name
 		this.name = name;
+		
+		// Set the community password
+		this.password = password;
 
 		// Create an empty collection of users
 		members = new ArrayList<User>();
 
 	}
 
-	public Community(final String name) {
-		this(new CommunityName(name));
+	public Community(final String name, final String password) {
+		this(new CommunityName(name), new Password(password));
 	}
 	
 	/**
@@ -43,4 +47,8 @@ public class Community implements Identifiable<CommunityName>, Serializable {
 		return members;
 	}
 
+	public boolean checkPassword(final String attempt) {
+		return password.checkPassword(attempt);
+	}
+	
 }

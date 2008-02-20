ed045cd4e5c0f0a49dbb5f1baf36f14ad326ad32
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-20 20:02:48 +0000

    checkstyle

--- trunk/colab/colab/util/StringUtils.java
+++ trunk/colab/colab/util/StringUtils.java
@@ -1,18 +1,36 @@
 package colab.util;
 
+/**
+ * A utility class containing methods for dealing with strings.
+ */
 public final class StringUtils {
 
     /** Hidden default constructor. */
     private StringUtils() {
-        ;
     }
-    
+
+    /**
+     * Returns a given string, or an empty string if null.
+     *
+     * @param str a string
+     * @return str, or an empty string if str is null
+     */
     public static String emptyIfNull(final String str) {
-        return (str != null) ? str : "";
+        if (str == null) {
+            return "";
+        } else {
+            return str;
+        }
     }
-    
+
+    /**
+     * Determines whether a string is empty or null.
+     *
+     * @param str a stirng
+     * @return true if the string is empty or null, false otherwise
+     */
     public static boolean isEmptyOrNull(final String str) {
         return (str == null) || (str.equals(""));
     }
-    
+
 }

cc9235b4ed78fd333ad5a60fb916fd100582aa64
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-20 19:54:41 +0000

    checkstyle

--- trunk/colab/colab/user/UserName.java
+++ trunk/colab/colab/user/UserName.java
@@ -2,12 +2,21 @@ package colab.user;
 
 import colab.identity.StringIdentifier;
 
+/**
+ * The name a user uses to log in.
+ */
 public class UserName extends StringIdentifier {
 
+    /** Serialization version number. */
     public static final long serialVersionUID = 1L;
-    
+
+    /**
+     * Constructs a new user name.
+     *
+     * @param name the name of a user
+     */
     public UserName(final String name) {
         super(name);
     }
-    
+
 }

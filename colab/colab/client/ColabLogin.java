ea0af5ca8906d2e5a7e5e47b9b6f61c62be30ec6
dancepants <dancepants@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-23 19:03:28 +0000

    LoginPanel window is better formatted; ChooseCommunityPanel pops up after user hits submit (no testing of user name or server ip yet implemented) with a drop down menu to choose the community from the list (not yet populated).

--- colab/colab/client/ColabLogin.java
+++ colab/colab/client/ColabLogin.java
@@ -3,6 +3,8 @@ package colab.client;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
 
 public class ColabLogin implements ActionListener {
 
@@ -13,6 +15,15 @@ public class ColabLogin implements ActionListener {
 
     public void actionPerformed(final ActionEvent arg0) {
         // TODO Auto-generated method stub
+    	JFrame f = new JFrame("Select Community");
+        ChooseCommunityPanel cPanel = new ChooseCommunityPanel();
+
+        f.add(cPanel);
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(400, 100);
+        f.setVisible(true);
+    	
     }
 
 }

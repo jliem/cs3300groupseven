c9bc377e147196b0991484f55087077d8f169b99
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-23 06:37:01 +0000

    add test cases for Password

---
+++ trunk/colab-test/colab/common/user/PasswordTester.java
@@ -0,0 +1,26 @@
+package colab.common.user;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link Password}.
+ */
+public final class PasswordTester extends TestCase {
+
+    /**
+     * Creates a password, then checks the correct password.
+     */
+    public void testCorrectPassword() {
+        Password p = new Password("jesus");
+        assertTrue(p.checkPassword("jesus"));
+    }
+
+    /**
+     * Creates a password, then checks an incorrect password.
+     */
+    public void testIncorrectPassword() {
+        Password p = new Password("jesus");
+        assertFalse(p.checkPassword("buddha"));
+    }
+
+}

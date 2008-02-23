3d96183dcd9565c8a55e2043baae71257efb4a9d
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-23 06:49:24 +0000

    documentation and checkstyle

--- trunk/colab/colab/common/channel/ChatChannelData.java
+++ trunk/colab/colab/common/channel/ChatChannelData.java
@@ -1,20 +1,35 @@
 package colab.common.channel;
 
+import java.util.Date;
+
 import colab.common.user.User;
 
-public class ChatChannelData extends ChannelData {
+/**
+ * Represents a message posted to a chat channel.
+ */
+public final class ChatChannelData extends ChannelData {
 
-    //private String text;
-    
-    public ChatChannelData(String text, User creator) {
+    /** The text of the message. */
+    private String text;
 
-        //this.text = text;
-        this.creator = creator;
-        
-        // TODO: Set timestamp
-
+    /**
+     * Constructs a new chat data object.
+     *
+     * @param text the text of the message
+     * @param creator the user who posted the message
+     */
+    public ChatChannelData(final String text, final User creator) {
+        super(creator, new Date());
+        this.text = text;
     }
 
-    
-    
+    /**
+     * Returns the contents of the message.
+     *
+     * @return the textual message
+     */
+    public String getText() {
+        return this.text;
+    }
+
 }

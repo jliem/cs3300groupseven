3ca4be5e8023880823154bf2f0f9cce26dd44551
mhluongo <mhluongo@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-24 21:58:19 +0000

    This- is pretty much the sweetest class ever.
    Love,
    
    Matt

--- trunk/colab/colab/client/gui/ChannelPanel.java
+++ trunk/colab/colab/client/gui/ChannelPanel.java
@@ -1,58 +1,84 @@
 package colab.client.gui;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Vector;
 
-import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 import colab.common.channel.ChannelDescriptor;
 
 public class ChannelPanel extends JPanel {
-  
-  private final LinkedList<ChannelDescriptor> pendingJoins;
-  private final ArrayList<ChannelDescriptor> channels;
-  
-  private final JList channelList ;
-  
-  public ChannelPanel()
-  {
-    pendingJoins = new LinkedList<ChannelDescriptor>();
-    channels = new ArrayList<ChannelDescriptor>();
-    
-    channelList = new JList();
-    JScrollPane listScroll = new JScrollPane(channelList);
-    
-    listScroll.setPreferredSize(new Dimension(100, 230));
-    setPreferredSize(new Dimension(100, 275));
-    
-    add(listScroll);
-  }
-  
-  public void addChannel(ChannelDescriptor chan)
-  {
-      channels.add(chan);
-      
-  }
-  
-//  public void addChannel(ChannelDescriptor chan)
-//  {
-//      
-//  }
-  
-  public static void main(final String args[]){
-      JFrame f = new JFrame();
-      f.setLayout(new FlowLayout());
-      ChannelPanel chan = new ChannelPanel();
-      ChatPanel chat = new ChatPanel("test");
-      f.add(chan);
-      f.add(chat);
-      f.setSize(425, 275);
-      f.setVisible(true);
-      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
+
+    private final ArrayList<ActionListener> listeners;
+
+    private final LinkedList<ChannelDescriptor> pendingJoins;
+
+    private final Vector<ChannelDescriptor> channels;
+
+    private final JList channelList;
+
+    public ChannelPanel() {
+        listeners = new ArrayList<ActionListener>();
+
+        pendingJoins = new LinkedList<ChannelDescriptor>();
+        channels = new Vector<ChannelDescriptor>();
+
+        channelList = new JList(channels);
+
+        channelList.setPreferredSize(new Dimension(100, 230));
+        setPreferredSize(new Dimension(100, 275));
+
+        channelList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        channelList.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() > 1) {
+                    // adds the currently selected channel descriptor to our
+                    // pending join channel list
+                    pendingJoins.add(channels.elementAt(channelList
+                            .getLeadSelectionIndex()));
+                    fireActionPerformed(new ActionEvent(this,
+                            ActionEvent.ACTION_FIRST, "Joined"));
+                }
+            }
+        });
+
+        add(channelList);
+    }
+
+    public void addChannel(ChannelDescriptor chan) {
+        channels.add(chan);
+    }
+
+    public void removeChannel(ChannelDescriptor chan) {
+        channels.remove(chan);
+    }
+
+    public ChannelDescriptor popJoinedChannel() {
+        if (pendingJoins.size() > 0) {
+            return pendingJoins.removeFirst();
+        }
+        return null;
+    }
+
+    public void addActionListener(ActionListener l) {
+        listeners.add(l);
+    }
+
+    public void removeActionListener(ActionListener l) {
+        listeners.remove(l);
+    }
+
+    public void fireActionPerformed(ActionEvent e) {
+        for (ActionListener l : listeners) {
+            l.actionPerformed(e);
+        }
+    }
 }

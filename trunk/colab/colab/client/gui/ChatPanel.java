bf448723f962bd0c226a032ab4e46bb9e685d07a
dancepants <dancepants@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-24 21:13:54 +0000

    logout!

--- trunk/colab/colab/client/gui/ChatPanel.java
+++ trunk/colab/colab/client/gui/ChatPanel.java
@@ -11,96 +11,119 @@ import java.util.LinkedList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class ChatPanel extends JPanel {
 
-  private ArrayList<ActionListener> listeners;
-  private final String user;
-  private final JTextArea textArea, textbox;
-  private LinkedList <String> pendingMessages;
+    private ArrayList<ActionListener> listeners;
+    private final String user;
+    private final JButton logoutButton, switchButton;
+    private final JTextArea textArea, textbox;
+    private LinkedList<String> pendingMessages;
 
-  public ChatPanel(String username) {
-    listeners = new ArrayList<ActionListener>();
+    public ChatPanel(String username) {
+        listeners = new ArrayList<ActionListener>();
+        logoutButton = new JButton("Logout");
+        switchButton = new JButton("Switch Communities");
+        textArea = new JTextArea(10, 30);
+        textbox = new JTextArea(2, 20);
 
-    textArea = new JTextArea(10, 30);
-    textbox = new JTextArea(2, 20);
+        user = new String(username);
+        pendingMessages = new LinkedList<String>();
 
-    user = new String(username);
-    pendingMessages = new LinkedList<String>();
+        JScrollPane textScroll = new JScrollPane(textArea);
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
 
-    JScrollPane textScroll = new JScrollPane(textArea);
-    textArea.setLineWrap(true);
-    textArea.setEditable(false);
+        JScrollPane sendScroll = new JScrollPane(textbox);
+        textbox.setLineWrap(true);
 
-    JScrollPane sendScroll = new JScrollPane(textbox);
-    textbox.setLineWrap(true);
+        textbox.addKeyListener(new KeyAdapter() {
+            public void keyPressed(final KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (e.isShiftDown())
+                        textbox.append("\n");
+                    else {
+                        writeMessage(user + ": " + textbox.getText());
+                        pendingMessages
+                                .addLast(user + ": " + textbox.getText());
+                        textbox.setText("");
+                        fireActionPerformed(new ActionEvent(this,
+                                ActionEvent.ACTION_FIRST, "Message Sent"));
+                    }
+                }
+            }
+        });
 
-    textbox.addKeyListener(new KeyAdapter()
-    {
-      public void keyPressed(final KeyEvent e){
-        if(e.getKeyCode() == KeyEvent.VK_ENTER){
-          if(e.isShiftDown())
-            textbox.append("\n");
-          else{
-            writeMessage(user + ": " + textbox.getText());
-            pendingMessages.addLast(user + ": " + textbox.getText());
-            textbox.setText("");
-            fireActionPerformed(new ActionEvent(this, ActionEvent.ACTION_FIRST,
-                    "Message Sent"));
-          }
-        }
-      }
-    });
+        textScroll.setPreferredSize(new Dimension(300, 200));
+        sendScroll.setPreferredSize(new Dimension(300, 25));
 
-    textScroll.setPreferredSize(new Dimension(300, 200));
-    sendScroll.setPreferredSize(new Dimension(300, 25));
+        setPreferredSize(new Dimension(300, 275));
+        setLayout(new FlowLayout());
 
-    setPreferredSize(new Dimension(300, 275));
-    setLayout(new FlowLayout());
+        logoutButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e){
+                fireActionPerformed(new ActionEvent(this,
+                        ActionEvent.ACTION_FIRST, "Logout!"));
+            }
+        });
 
-    add(textScroll);
-    add(sendScroll);
-  }
+        switchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e){
+                fireActionPerformed(new ActionEvent(this,
+                        ActionEvent.ACTION_FIRST, "Switch!"));
+            }
+        });
 
-  /**
-   * Adds new chat input to original chat dialog.
-   * @param mess from the user
-   */
-  public final void writeMessage(final String mess){
-    textArea.append(mess + "\n");
-  }
-
- /**
-  * Adds an ActionListener of a certain type to an element.
-  * @param l the particular ActionListener
-  */
-  public final void addActionListener(final ActionListener l){
-    listeners.add(l);
-  }
-
-  /**
-   * Fires when ActionEvent e occurs.
-   * @param e the ActionEvent that occurs
-   */
-  protected final void fireActionPerformed(final ActionEvent e) {
-    for (ActionListener l : listeners) {
-      l.actionPerformed(e);
+        add(textScroll);
+        add(sendScroll);
+        add(logoutButton);
+        add(switchButton);
     }
-  }
 
-  /**
-   * Displays a ChatPanel.
-   * @param args standard
-   */
-  public static void main(final String[] args) {
-    ChatPanel p = new ChatPanel("test!");
-    JFrame f = new JFrame();
-    f.add(p);
-    f.setSize(325, 275);
-    f.setResizable(false);
-    f.setVisible(true);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
+    /**
+     * Adds new chat input to original chat dialog.
+     * @param mess
+     *            from the user
+     */
+    public final void writeMessage(final String mess) {
+        textArea.append(mess + "\n");
+    }
+
+    /**
+     * Adds an ActionListener of a certain type to an element.
+     * @param l
+     *            the particular ActionListener
+     */
+    public final void addActionListener(final ActionListener l) {
+        listeners.add(l);
+    }
+
+    /**
+     * Fires when ActionEvent e occurs.
+     * @param e
+     *            the ActionEvent that occurs
+     */
+    protected final void fireActionPerformed(final ActionEvent e) {
+        for (ActionListener l : listeners) {
+            l.actionPerformed(e);
+        }
+    }
+
+    /**
+     * Displays a ChatPanel.
+     * @param args
+     *            standard
+     */
+    public static void main(final String[] args) {
+        ChatPanel p = new ChatPanel("test!");
+        JFrame f = new JFrame();
+        f.add(p);
+        f.setSize(350, 355);
+        f.setResizable(false);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }

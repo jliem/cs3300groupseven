6d13d43e87cb9195dc023847b7be4cb6b35c13ad
dancepants <dancepants@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-24 04:20:24 +0000

    Mo bettah ui with new keylistener feature!

--- trunk/colab/colab/client/gui/LoginPanel.java
+++ trunk/colab/colab/client/gui/LoginPanel.java
@@ -1,12 +1,18 @@
 package colab.client.gui;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
+import colab.client.ColabClient;
 
 public class LoginPanel extends JPanel {
 
@@ -17,10 +23,14 @@ public class LoginPanel extends JPanel {
     private JPasswordField password;
     private JTextField username, serverIP;
     private JButton loginButton;
-    //private
-
-    public LoginPanel(){
-
+    protected boolean loginOK;
+    
+    private ArrayList<ActionListener> listeners;
+    
+    private static int eventID = 1;
+    
+    public LoginPanel(final ColabClient client)
+    {
         usernameLabel = new JLabel("Username / Desired Username: ");
         passwordLabel = new JLabel("Password / Desired Password: ");
         serverLabel = new JLabel("Enter server IP: ");
@@ -28,11 +38,51 @@ public class LoginPanel extends JPanel {
         password = new JPasswordField(12);
         serverIP = new JTextField(32);
         loginButton = new JButton("Submit");
+      
+        listeners = new ArrayList<ActionListener>();
         
+       
+        
+        KeyListener k = new KeyListener()
+        {
+            
+            public void keyPressed(KeyEvent arg0) {
+                // TODO Auto-generated method stub
+                if(arg0.getKeyChar() == KeyEvent.VK_ENTER){
+                    if(client.loginUser(username.getText(), password.getPassword(), serverIP.getText()))
+                        fireActionPerformed(new ActionEvent(this, eventID++, "Login Succeeded!"));
+                    else
+                        fireActionPerformed(new ActionEvent(this, eventID++, "Login Failed!"));
+                }
+            }
 
-        ColabLogin login = new ColabLogin(
-                username.getText(), password.getPassword(), serverIP.getText());
-        loginButton.addActionListener(login);
+            public void keyReleased(KeyEvent arg0) {
+                // TODO Auto-generated method stub
+                
+            }
+
+            public void keyTyped(KeyEvent arg0) {
+                // TODO Auto-generated method stub
+                
+                
+            }
+        };
+        
+        username.addKeyListener(k);
+        password.addKeyListener(k);
+        serverIP.addKeyListener(k);
+        
+        
+        loginButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                if(client.loginUser(username.getText(), password.getPassword(), serverIP.getText()))
+                    fireActionPerformed(new ActionEvent(this, eventID++, "Login Succeeded!"));
+                else
+                    fireActionPerformed(new ActionEvent(this, eventID++, "Login Failed!"));
+            }
+        });
 
         setLayout(new GridLayout(4, 2));
 
@@ -43,19 +93,18 @@ public class LoginPanel extends JPanel {
         add(passwordLabel);
         add(password);
         add(loginButton);
-
+        
+       
     }
 
-    public static void main(final String[] args) {
-        JFrame f = new JFrame("CoLab Login");
-        LoginPanel loginPanel = new LoginPanel();
-
-        f.add(loginPanel);
-
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(400, 100);
-        f.setVisible(true);
-
+    protected void fireActionPerformed(ActionEvent e)
+    {
+        for(ActionListener l:listeners)
+            l.actionPerformed(e);
     }
-
+    
+    public void addActionListener(ActionListener listener)
+    {
+        listeners.add(listener);
+    }
 }

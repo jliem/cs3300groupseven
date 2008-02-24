7d01844b68df33fe14891cef4040c454f0dc828b
dancepants <dancepants@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-24 04:20:24 +0000

    Mo bettah ui with new keylistener feature!

--- colab/colab/client/ColabClient.java
+++ colab/colab/client/ColabClient.java
@@ -5,6 +5,7 @@ import java.rmi.Naming;
 import colab.common.channel.ChannelName;
 import colab.common.channel.remote.ChannelInterface;
 import colab.common.community.CommunityName;
+import colab.common.user.Password;
 import colab.common.user.UserName;
 import colab.server.remote.ColabServerInterface;
 import colab.server.remote.ConnectionInterface;
@@ -16,6 +17,8 @@ public final class ColabClient {
 
     /** The default port. */
     public static final int DEFAULT_PORT = 9040;
+    private String username; 
+    private Password password;
 
     /**
      * Constructs the client application.
@@ -25,12 +28,51 @@ public final class ColabClient {
     }
 
     /**
-     * The entry point which launches the client application.
-     *
-     * @param args unused
-     * @throws Exception if an error occurs in rmi setup
+     * Receives the server IP from the GUI and attempts to connect to said
+     * server.
+     * @param serverIP address
+     * @return whether or not the client connected to the server
      */
-    public static void main(final String[] args) throws Exception {
+    public boolean connect(final String serverIP) {
+
+        return true;
+
+    }
+
+    /**
+     * Receives the username and password from the GUI fields and checks to
+     * see if there is an existing corresponding pair.  If the username
+     * exists but the password is incorrect, a ValidationException is thrown.
+     * If the username does not exist, then the GUI asks the user if a new
+     * user should be added.
+     * @param username received from the GUI text field
+     * @param password received from the GUI password field
+     * @param serverIP address received from the GUI text field
+     * @return whether or not the user is logged in
+     */
+
+    public boolean loginUser(final String username, final char[] password,
+            final String serverIP)
+    {
+        
+        if(connect(serverIP))
+        {
+            this.username = username;
+            this.password = new Password(password);
+            //compare to server
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * The entry point which launches the client application.
+     * @param args unused
+     * @throws Exception
+     *             if an error occurs in rmi setup
+     */
+    /*
+ public static void main(final String[] args) throws Exception {
 
         // Assign a security manager, in the event
         // that dynamic classes are loaded.
@@ -62,5 +104,6 @@ public final class ColabClient {
         }
 
     }
+    */
 
 }
--- colab/colab/client/gui/ColabClientGUI.java
+++ colab/colab/client/gui/ColabClientGUI.java
@@ -1,22 +1,47 @@
 package colab.client.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 
 import colab.client.ColabClient;
 
 public class ColabClientGUI extends JFrame {
 
-    public ColabClientGUI(final ColabClient client) {
-
+    private ColabClient client;
+	
+	public ColabClientGUI(final ColabClient client) {
+    	
+		this.client = client;
     }
 
-    public static void main(final String[] args) {
-
+    public static void main(final String[] args)
+    {
         ColabClient client = new ColabClient();
-
-        ColabClientGUI gui = new ColabClientGUI(client);
+        final LoginPanel loginPanel = new LoginPanel(client);
+        final ColabClientGUI gui = new ColabClientGUI(client);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setTitle("CoLab Login");
         gui.setVisible(true);
-
+        gui.setSize(400, 100);
+        gui.add(loginPanel);
+        
+        loginPanel.addActionListener(new ActionListener()
+        {
+           public void actionPerformed(ActionEvent e)
+           {
+               if(e.getActionCommand().equals("Login Succeeded!")){
+                   ChooseCommunityPanel commPanel = new ChooseCommunityPanel();
+                   gui.remove(loginPanel);
+                   gui.add(commPanel);
+                   gui.setTitle("Select Community");
+                   commPanel.updateUI();
+                   
+               }
+           }
+        });
     }
+    
 
 }

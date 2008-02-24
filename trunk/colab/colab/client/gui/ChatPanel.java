3c95e37e02b05e3bd3eb2b18b15004f690d23aa2
dancepants <dancepants@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-24 15:32:18 +0000

    Better format (according to CheckStyle) and a few code tweaks.

--- trunk/colab/colab/client/gui/ChatPanel.java
+++ trunk/colab/colab/client/gui/ChatPanel.java
@@ -14,81 +14,87 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class ChatPanel extends JPanel
-{
+public class ChatPanel extends JPanel{
   private ArrayList<ActionListener> listeners;
-  
   private final String user;
   private final JTextArea textArea, textbox;
-  
-  private static int eventID = 0;
   private LinkedList <String> pendingMessages;
-  
-  public ChatPanel(String username)
-  {
+
+  public ChatPanel(String username){
     listeners = new ArrayList<ActionListener>();
-    
+
     textArea = new JTextArea(10, 30);
     textbox = new JTextArea(2, 20);
-    
+
     user = new String(username);
     pendingMessages = new LinkedList<String>();
-    
+
     JScrollPane textScroll = new JScrollPane(textArea);
     textArea.setLineWrap(true);
     textArea.setEditable(false);
-    
+
     JScrollPane sendScroll = new JScrollPane(textbox);
-    textbox.setLineWrap(true); 
-    
+    textbox.setLineWrap(true);
+
     textbox.addKeyListener(new KeyAdapter()
     {
-      public void keyPressed(KeyEvent e)
-      {
-        if(e.getKeyCode() == KeyEvent.VK_ENTER)
+      public void keyPressed(final KeyEvent e){
+        if(e.getKeyCode() == KeyEvent.VK_ENTER){
           if(e.isShiftDown())
-          {
             textbox.append("\n");
-          }
-          else
-          {
+          else{
             writeMessage(user + ": " + textbox.getText());
             pendingMessages.addLast(user + ": " + textbox.getText());
             textbox.setText("");
-            fireActionPerformed(new ActionEvent(this, eventID++, "Message Sent"));
+            fireActionPerformed(new ActionEvent(this, ActionEvent.ACTION_FIRST,
+                    "Message Sent"));
           }
+        }
       }
     });
-    
+
     textScroll.setPreferredSize(new Dimension(300, 200));
     sendScroll.setPreferredSize(new Dimension(300, 25));
-    
-    
+
     setPreferredSize(new Dimension(300, 275));
     setLayout(new FlowLayout());
-    
+
     add(textScroll);
     add(sendScroll);
   }
-  
-  public void writeMessage(String mess)
-  {
+
+  /**
+   * Adds new chat input to original chat dialog.
+   * @param mess from the user
+   */
+  public final void writeMessage(final String mess){
     textArea.append(mess + "\n");
   }
-  
-  public void addActionListener(ActionListener l)
-  {
+
+ /**
+  * Adds an ActionListener of a certain type to an element.
+  * @param l the particular ActionListener
+  */
+  public final void addActionListener(final ActionListener l){
     listeners.add(l);
   }
-  
-  protected void fireActionPerformed(ActionEvent e)
-  {
-    for(ActionListener l: listeners)
+
+  /**
+   * Fires when ActionEvent e occurs.
+   * @param e the ActionEvent that occurs
+   */
+  protected final void fireActionPerformed(final ActionEvent e) {
+    for (ActionListener l : listeners) {
       l.actionPerformed(e);
+    }
   }
-  
-  public static void main(String args[])
-  {
+
+  /**
+   * Displays a ChatPanel.
+   * @param args standard
+   */
+
+  public static void main(final String[] args) {
     ChatPanel p = new ChatPanel("test!");
     JFrame f = new JFrame();
     f.add(p);

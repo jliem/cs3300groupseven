1475da49c7c62a0a50036e5708377095d2a13468
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-26 02:23:38 +0000

    comment

--- colab/colab/common/remote/client/ColabClientInterface.java
+++ colab/colab/common/remote/client/ColabClientInterface.java
@@ -5,9 +5,22 @@ import java.rmi.RemoteException;
 
 import colab.common.channel.ChannelDescriptor;
 
+/**
+ * A remote interface for a client-side object which the
+ * server application uses when it needs to update the client
+ * with some information that is not specific to any
+ * particular channel.
+ */
 public interface ColabClientInterface extends Remote {
 
-    public void channelAdded(ChannelDescriptor channelDescriptor)
+    /**
+     * Notifies the client that a channel exists in the community
+     * in which the user is logged in.
+     *
+     * @param channelDescriptor the channel
+     * @throws RemoteException if an rmi error occurs
+     */
+    void channelAdded(ChannelDescriptor channelDescriptor)
             throws RemoteException;
 
 }

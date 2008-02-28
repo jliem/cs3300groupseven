4ab5fe861a50557097ea6315c2a2880a25f324d9
mhluongo <mhluongo@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-02-28 07:57:05 +0000

    This revision includes most of the functioning export behavior, a few minor GUI fixes, and an increased importance for ChatChannelData. Client-side chat no longer passes strings, but now fully supports and expects ChatChannelData objects.  In addition, messages are no longer sent "pre-concatenated" to the server- that is, a ChatChannelData's text will not reader "<username>: <message>", to eliminate overlap with the object's creator field.  A shortcut method to compose client-side strings for output can be found in ChatChannelData, called getMessageString.
    This revision also fuller uses timestamps, and the next will allow a 
    user to turn GUI timestamping on or off.
    
    - Matt

--- trunk/colab/colab/client/gui/ChatChannelFrame.java
+++ trunk/colab/colab/client/gui/ChatChannelFrame.java
@@ -3,60 +3,134 @@ package colab.client.gui;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.rmi.RemoteException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 import colab.client.ClientChatChannel;
 import colab.client.ColabClient;
 import colab.common.channel.ChatChannelData;
+import colab.common.naming.ChannelName;
 import colab.common.naming.UserName;
 
 public class ChatChannelFrame extends JFrame {
-    private final ColabClient client;
-    private final ClientChatChannel channel;
-    private final ChatPanel chatPanel;
-    
-    private static final long serialVersionUID = 1;
-    
-    public ChatChannelFrame(final ColabClient client,
-            ClientChatChannel clientChannel, final UserName name) {
-        this.client = client;
-        channel = clientChannel;
-        chatPanel = new ChatPanel(name);
+	private final ColabClient client;
 
-        chatPanel.addActionListener(new ActionListener() {
-           public void actionPerformed(final ActionEvent e) {
-               String mess;
-               while((mess = chatPanel.dequeuePendingMessage()) != null) {
-                   try
-                   {
-                       client.add(channel.getId(), new ChatChannelData(mess, name));
-                   }
-                   catch(RemoteException ex)
-                   {
-//                       REALLY CUTESY FLAG FOR CHRIS!!!!!!!!!!!!!!!!!!
-//                       ~ <(^.^)> ~
-                   }
-               }
+	private final ClientChatChannel channel;
 
-           }
-        });
+	private final ChatPanel chatPanel;
 
-        channel.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent e) {
-               for(String mess : channel.getNewMessages()) {
-                   chatPanel.writeMessage(mess);
-               }
-           }
-        });
+	private final JMenuBar menu;
 
+	private final JMenu fileMenu;
 
-        setTitle(channel.getId().toString());
-        setSize(new Dimension(320, 300));
+	private final JMenuItem export, exit;
 
-        setContentPane(chatPanel);
-    }
+	private static final long serialVersionUID = 1;
+
+	public ChatChannelFrame(final ColabClient client,
+			ClientChatChannel clientChannel, final UserName name) {
+		this.client = client;
+		channel = clientChannel;
+		chatPanel = new ChatPanel(name);
+
+		chatPanel.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent e) {
+				String mess;
+				while ((mess = chatPanel.dequeuePendingMessage()) != null) {
+					try {
+						client.add(channel.getId(), new ChatChannelData(mess,
+								name));
+					} catch (RemoteException ex) {
+						// REALLY CUTESY FLAG FOR CHRIS!!!!!!!!!!!!!!!!!!
+						// ~ <(^.^)> ~
+					}
+				}
+
+			}
+		});
+
+		channel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for (ChatChannelData mess : channel.getNewMessages()) {
+					chatPanel.writeMessage(mess);
+				}
+			}
+		});
+
+		menu = new JMenuBar();
+		fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+
+		export = new JMenuItem("Export Chat");
+		export.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E,
+				ActionEvent.ALT_MASK));
+		export.getAccessibleContext().setAccessibleDescription(
+				"Exports the conversation to a text file.");
+		exit = new JMenuItem("Exit");
+		exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
+				ActionEvent.ALT_MASK));
+		exit.getAccessibleContext().setAccessibleDescription(
+				"Leaves the channel.");
+
+		export.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ExportChatFrame frame = new ExportChatFrame(channel);
+				frame.setVisible(true);
+			}
+		});
+
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				exit();
+			}
+		});
+
+		fileMenu.add(export);
+		fileMenu.add(exit);
+
+		menu.add(fileMenu);
+
+		this.setJMenuBar(menu);
+
+		setTitle(channel.getId().toString());
+		setSize(new Dimension(320, 300));
+
+		// and since we'd like the channel to be exited on close...
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				exit();
+			}
+		});
+
+		add(chatPanel);
+	}
+
+	protected void exit() {
+		// TODO:add "leave channel" code
+		setVisible(false);
+		dispose();
+	}
+
+	public static void main(String args[]) throws RemoteException {
+		ChatChannelFrame chat = new ChatChannelFrame(new ColabClient(),
+				new ClientChatChannel(new ChannelName("Test Channel")),
+				new UserName("test"));
+		chat.setVisible(true);
+	}
+
+	public boolean isTimestampEnabled() {
+		return chatPanel.isTimestampEnabled();
+	}
+
+	public void setTimestampEnabled(boolean timestampEnabled) {
+		chatPanel.setTimestampEnabled(timestampEnabled);
+	}
 }
--- trunk/colab/colab/client/gui/ChatPanel.java
+++ trunk/colab/colab/client/gui/ChatPanel.java
@@ -2,7 +2,6 @@ package colab.client.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
@@ -18,136 +17,156 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import colab.common.channel.ChatChannelData;
 import colab.common.naming.UserName;
 
 class ChatPanel extends JPanel {
 
 	public static final long serialVersionUID = 1;
 
-    private ArrayList<ActionListener> listeners;
-    private final String user;
-    private final JScrollPane chatScroll, sendScroll;
-    private final JTextArea textArea, textbox;
-    private LinkedList<String> pendingMessages;
+	private ArrayList<ActionListener> listeners;
 
-    private Dimension lastSize;
+	private final String user;
 
-    public ChatPanel(UserName name) {
-        listeners = new ArrayList<ActionListener>();
-        textArea = new JTextArea(10, 30);
-        textbox = new JTextArea(2, 20);
+	private final JScrollPane chatScroll, sendScroll;
 
-        user = new String(name.toString());
-        pendingMessages = new LinkedList<String>();
+	private final JTextArea textArea, textbox;
 
-        chatScroll = new JScrollPane(textArea);
-        textArea.setLineWrap(true);
-        textArea.setEditable(false);
+	private LinkedList<String> pendingMessages;
 
-        sendScroll = new JScrollPane(textbox);
-        textbox.setLineWrap(true);
+	private Dimension lastSize;
 
-        textbox.addKeyListener(new KeyAdapter() {
-            public void keyPressed(final KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    if (e.isShiftDown()) {
-                        textbox.append("\n");
-                    } else {
-                        if (!textbox.getText().matches("\\A\\s*\\z")) {
-                            writeMessage(user + ": " + textbox.getText());
-                            pendingMessages
-                                    .addLast(user + ": " + textbox.getText());
-                            textbox.setText("");
-                            fireActionPerformed(new ActionEvent(this,
-                                    ActionEvent.ACTION_FIRST, "Message Sent"));
-                        }
-                    }
-                    e.consume();
-                }
-            }
-        });
+	private boolean timestampEnabled = false;
 
-        addComponentListener(new ComponentAdapter(){
-        	public void componentResized(ComponentEvent e){
-        		Dimension size = getSize();
+	public ChatPanel(UserName name) {
+		listeners = new ArrayList<ActionListener>();
+		textArea = new JTextArea(10, 30);
+		textbox = new JTextArea(2, 20);
 
-        		Dimension chatSize = new Dimension((int)(size.getWidth()-10), (int)(size.getHeight()-40)),
-        			sendSize = new Dimension((int)(size.getWidth()-10), 25);
+		user = new String(name.toString());
+		pendingMessages = new LinkedList<String>();
 
-        		chatScroll.setPreferredSize(chatSize);
-        		sendScroll.setPreferredSize(sendSize);
-        	}
-        });
+		chatScroll = new JScrollPane(textArea);
+		textArea.setLineWrap(true);
+		textArea.setEditable(false);
 
-        chatScroll.setPreferredSize(new Dimension(300, 235));
-        sendScroll.setPreferredSize(new Dimension(300, 25));
+		sendScroll = new JScrollPane(textbox);
+		textbox.setLineWrap(true);
 
-        setPreferredSize(new Dimension(300, 275));
-        setLayout(new BorderLayout());
+		textbox.addKeyListener(new KeyAdapter() {
+			public void keyPressed(final KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					if (e.isShiftDown()) {
+						textbox.append("\n");
+					} else {
+						if (!textbox.getText().matches("\\A\\s*\\z")) {
+							// additional "write message" functionality
+							// TODO: should probably be refactored to own method
+							textArea.append(user + ": " + textbox.getText()
+									+ "\n");
+							pendingMessages.addLast(textbox.getText());
+							textbox.setText("");
+							fireActionPerformed(new ActionEvent(this,
+									ActionEvent.ACTION_FIRST, "Message Sent"));
+						}
+					}
+					e.consume();
+				}
+			}
+		});
 
+		addComponentListener(new ComponentAdapter() {
+			public void componentResized(ComponentEvent e) {
+				Dimension size = getSize();
 
-        add(chatScroll, BorderLayout.CENTER);
-        add(sendScroll, BorderLayout.SOUTH);
+				Dimension chatSize = new Dimension(
+						(int) (size.getWidth() - 10),
+						(int) (size.getHeight() - 40)), sendSize = new Dimension(
+						(int) (size.getWidth() - 10), 25);
 
+				chatScroll.setPreferredSize(chatSize);
+				sendScroll.setPreferredSize(sendSize);
+			}
+		});
 
-        lastSize = getSize();
-    }
+		chatScroll.setPreferredSize(new Dimension(300, 235));
+		sendScroll.setPreferredSize(new Dimension(300, 25));
 
-    /**
-     * Adds new chat input to original chat dialog.
-     * @param mess
-     *            from the user
-     */
-    public final void writeMessage(final String mess) {
-        textArea.append(mess + "\n");
+		setPreferredSize(new Dimension(300, 275));
+		setLayout(new BorderLayout());
 
-        JScrollBar bar = chatScroll.getVerticalScrollBar();
-        boolean autoScroll = ((bar.getValue() + bar.getVisibleAmount()) == bar.getMaximum());
+		add(chatScroll, BorderLayout.CENTER);
+		add(sendScroll, BorderLayout.SOUTH);
 
-        if( autoScroll ) {
-        	textArea.setCaretPosition( textArea.getDocument().getLength() );
-        }
-    }
+		lastSize = getSize();
+	}
 
-    public String dequeuePendingMessage()
-    {
-        if(pendingMessages.size()>0){
-            return pendingMessages.removeFirst();
-        }
-        return null;
-    }
+	/**
+	 * Adds new chat input to original chat dialog.
+	 * 
+	 * @param mess
+	 *            from the user
+	 */
+	public final void writeMessage(final ChatChannelData mess) {
+		textArea.append(mess.getMessageString(timestampEnabled) + "\n");
+		JScrollBar bar = chatScroll.getVerticalScrollBar();
+		boolean autoScroll = ((bar.getValue() + bar.getVisibleAmount()) == bar
+				.getMaximum());
 
-    /**
-     * Adds an ActionListener of a certain type to an element.
-     * @param l
-     *            the particular ActionListener
-     */
-    public final void addActionListener(final ActionListener l) {
-        listeners.add(l);
-    }
+		if (autoScroll) {
+			textArea.setCaretPosition(textArea.getDocument().getLength());
+		}
+	}
 
-    /**
-     * Fires when ActionEvent e occurs.
-     * @param e
-     *            the ActionEvent that occurs
-     */
-    protected final void fireActionPerformed(final ActionEvent e) {
-        for (ActionListener l : listeners) {
-            l.actionPerformed(e);
-        }
-    }
+	public String dequeuePendingMessage() {
+		if (pendingMessages.size() > 0) {
+			return pendingMessages.removeFirst();
+		}
+		return null;
+	}
 
-    /**
-     * Displays a ChatPanel.
-     * @param args
-     *            standard
-     */
-    public static void main(final String[] args) {
-        ChatPanel p = new ChatPanel(new UserName("test"));
-        JFrame f = new JFrame();
-        f.setContentPane(p);
-        f.setSize(320, 300);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+	/**
+	 * Adds an ActionListener of a certain type to an element.
+	 * 
+	 * @param l
+	 *            the particular ActionListener
+	 */
+	public final void addActionListener(final ActionListener l) {
+		listeners.add(l);
+	}
+
+	/**
+	 * Fires when ActionEvent e occurs.
+	 * 
+	 * @param e
+	 *            the ActionEvent that occurs
+	 */
+	protected final void fireActionPerformed(final ActionEvent e) {
+		for (ActionListener l : listeners) {
+			l.actionPerformed(e);
+		}
+	}
+
+	/**
+	 * Displays a ChatPanel.
+	 * 
+	 * @param args
+	 *            standard
+	 */
+	public static void main(final String[] args) {
+		ChatPanel p = new ChatPanel(new UserName("test"));
+		JFrame f = new JFrame();
+		f.setContentPane(p);
+		f.setSize(320, 300);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public boolean isTimestampEnabled() {
+		return timestampEnabled;
+	}
+
+	public void setTimestampEnabled(boolean timestampEnabled) {
+		this.timestampEnabled = timestampEnabled;
+	}
 }

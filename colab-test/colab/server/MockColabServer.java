5ea8770b2b627c8a585cf088921637b128786c25
JohannesLiem <JohannesLiem@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-03-24 04:31:08 +0000

    MockColabServer modified to use the new createCommunity methods

--- colab-test/colab/server/MockColabServer.java
+++ colab-test/colab/server/MockColabServer.java
@@ -1,10 +1,9 @@
 package colab.server;
 
-import colab.common.channel.ChannelDescriptor;
-import colab.common.channel.ChannelType;
-import colab.common.naming.ChannelName;
+import colab.common.naming.CommunityName;
 import colab.server.channel.ChannelManager;
 import colab.server.user.Community;
+import colab.server.user.Password;
 import colab.server.user.User;
 import colab.server.user.UserManager;
 
@@ -32,27 +31,42 @@ public final class MockColabServer extends ColabServer {
         // Populate a few test communities
 
         Community groupSeven = new Community("Group Seven", "sevenPass");
-        groupSeven.getMembers().add(johannes.getId());
-        groupSeven.getMembers().add(pamela.getId());
-        groupSeven.getMembers().add(matthew.getId());
-        groupSeven.getMembers().add(chris.getId());
-        userManager.addCommunity(groupSeven);
+        super.createCommunity(groupSeven.getId(),
+                groupSeven.getPassword());
+        super.addAsMember(johannes.getId(), groupSeven.getId());
+        super.addAsMember(pamela.getId(), groupSeven.getId());
+        super.addAsMember(matthew.getId(), groupSeven.getId());
+        super.addAsMember(chris.getId(), groupSeven.getId());
+
+//        Community groupSeven = new Community("Group Seven", "sevenPass");
+//        groupSeven.getMembers().add(johannes.getId());
+//        groupSeven.getMembers().add(pamela.getId());
+//        groupSeven.getMembers().add(matthew.getId());
+//        groupSeven.getMembers().add(chris.getId());
+//        userManager.addCommunity(groupSeven);
+//
+//
+//        teamAwesome.getMembers().add(chris.getId());
+//        userManager.addCommunity(teamAwesome);
 
         Community teamAwesome = new Community("Team Awesome", "awesomePass");
-        teamAwesome.getMembers().add(chris.getId());
-        userManager.addCommunity(teamAwesome);
+        super.createCommunity(teamAwesome.getId(),
+                teamAwesome.getPassword());
+        super.addAsMember(chris.getId(), teamAwesome.getId());
 
         Community noMembers = new Community("The No-Members Community", "abcd");
-        userManager.addCommunity(noMembers);
+        super.createCommunity(noMembers.getId(),
+                noMembers.getPassword());
 
-        // Add a lobby to each community
-
-        ChannelDescriptor lobbyDesc = new ChannelDescriptor(
-                 new ChannelName("Lobby"), ChannelType.CHAT);
-
-        channelManager.addChannel(groupSeven.getId(), lobbyDesc);
-        channelManager.addChannel(teamAwesome.getId(), lobbyDesc);
-        channelManager.addChannel(noMembers.getId(), lobbyDesc);
+//
+//        // Add a lobby to each community
+//
+//        ChannelDescriptor lobbyDesc = new ChannelDescriptor(
+//                 new ChannelName("Lobby"), ChannelType.CHAT);
+//
+//        channelManager.addChannel(groupSeven.getId(), lobbyDesc);
+//        channelManager.addChannel(teamAwesome.getId(), lobbyDesc);
+//        channelManager.addChannel(noMembers.getId(), lobbyDesc);
 
     }
 

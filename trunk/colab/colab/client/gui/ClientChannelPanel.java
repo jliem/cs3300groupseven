7d88a9985fa60aecc801a5bb351792ecbd6d8fb4
JohannesLiem <JohannesLiem@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-03-25 16:41:14 +0000

    Started Document panel and frame. Refactoring in ChatPanel.

--- trunk/colab/colab/client/gui/ClientChannelPanel.java
+++ trunk/colab/colab/client/gui/ClientChannelPanel.java
@@ -1,10 +1,48 @@
 package colab.client.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
+
+import colab.common.naming.UserName;
 
 /**
  * Parent class for all client-side channel panels.
  */
 abstract class ClientChannelPanel extends JPanel {
 
+
+    protected final UserName username;
+
+    private ArrayList<ActionListener> listeners;
+
+    public ClientChannelPanel(UserName username) {
+
+        this.username = username;
+
+        listeners = new ArrayList<ActionListener>();
+    }
+
+    /**
+     * Adds an ActionListener of a certain type to an element.
+     *
+     * @param listener the particular ActionListener
+     */
+    public final void addActionListener(final ActionListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Fires when an ActionEvent occurs.
+     *
+     * @param event the ActionEvent that occurs
+     */
+    protected final void fireActionPerformed(final ActionEvent event) {
+
+        for (final ActionListener listener : listeners) {
+            listener.actionPerformed(event);
+        }
+    }
 }

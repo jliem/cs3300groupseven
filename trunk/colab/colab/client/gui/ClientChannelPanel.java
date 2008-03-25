f23186b90a6913a46a8dfe21a49fbe804359769c
JohannesLiem <JohannesLiem@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-03-25 16:28:14 +0000

    ClientChannelFrame now handles the user list panel, so client channel GUIs only need to extend ClientChannelPanel. ChatChannelFrame is still pretty ugly and needs to be refactored (see issue 52)

--- trunk/colab/colab/client/gui/ClientChannelPanel.java
+++ trunk/colab/colab/client/gui/ClientChannelPanel.java
@@ -3,32 +3,8 @@ package colab.client.gui;
 import javax.swing.JPanel;
 
 /**
- * Parent class for all client-side channel panels. The child
- * class is responsible for adding the userListPanel to itself.
+ * Parent class for all client-side channel panels.
  */
 abstract class ClientChannelPanel extends JPanel {
-
-    /**
-     * A panel which contains a list of active users in the channel.
-     */
-    private final UserListPanel userListPanel;
-
-    /**
-     * Constructs a new ClientChannelPanel.
-     */
-    public ClientChannelPanel() {
-
-        userListPanel = new UserListPanel();
-
-    }
-
-    /**
-     * @return a panel which contains a list of active users in the channel
-     */
-    public final UserListPanel getUserListPanel() {
-
-        return userListPanel;
-
-    }
 
 }

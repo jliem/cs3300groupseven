6d718e0cf713abae83d787bb950bd0b01eb30485
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-03-31 04:27:49 +0000

    initial steps in replacing NameDatePair with Integer for ParagraphIdentifier

--- trunk/colab/colab/common/channel/DocumentDataSet.java
+++ trunk/colab/colab/common/channel/DocumentDataSet.java
@@ -2,4 +2,10 @@ package colab.common.channel;
 
 public class DocumentDataSet extends ChannelDataSet<DocumentChannelData> {
 
+    /** Serialization version number. */
+    private static final long serialVersionUID = 1L;
+
+    /* TODO: This is the class that needs to keep track of
+     * paragraph identifiers and assign them when INSERT data is added. */
+
 }

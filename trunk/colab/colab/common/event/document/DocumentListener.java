6ded0900a025e9188203d4eee832b97cf5289016
mhluongo <mhluongo@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-04-05 21:53:22 +0000

    I really need to start using Subclipse.
    - Matt

---
+++ trunk/colab/colab/common/event/document/DocumentListener.java
@@ -0,0 +1,28 @@
+package colab.common.event.document;
+
+import colab.common.channel.document.Document;
+import colab.common.channel.document.DocumentParagraph;
+import colab.common.identity.ParagraphIdentifier;
+
+/**
+ * A listener for multiple events on a Document.
+ * 
+ * @see Document
+ */
+public interface DocumentListener {
+
+    /**
+     * Called whenever a paragraph is inserted into the document.
+     *  
+     * @param offset - the index after which the paragraph was inserted
+     * @param paragraph - the paragraph that was inserted
+     */
+    void onInsert(int offset, DocumentParagraph paragraph);
+    
+    /**
+     * Called whenever a paragraph is deleted from this document.
+     * 
+     * @param id - the ParagraphIdentifier of the DocumentParagraph that was deleted
+     */
+    void onDelete(ParagraphIdentifier id);
+}

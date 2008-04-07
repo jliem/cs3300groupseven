cacb053457c25b5897803496d1ad3b76d6997873
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-04-07 20:32:18 +0000

    finishing up document xml

--- trunk/colab/colab/common/channel/document/diff/Applicable.java
+++ trunk/colab/colab/common/channel/document/diff/Applicable.java
@@ -1,12 +1,15 @@
 package colab.common.channel.document.diff;
 
+import java.io.Serializable;
+
 import colab.common.channel.document.DocumentParagraph;
 import colab.common.exception.NotApplicableException;
+import colab.common.xml.XmlSerializable;
 
 /**
  * The interface used to internally represent individual edits.
  */
-interface Applicable {
+interface Applicable extends Serializable, XmlSerializable {
 
     void apply(DocumentParagraph para) throws NotApplicableException;
 
--- trunk/colab/colab/common/channel/document/diff/ChangeLevel.java
+++ trunk/colab/colab/common/channel/document/diff/ChangeLevel.java
@@ -2,10 +2,18 @@ package colab.common.channel.document.diff;
 
 import colab.common.channel.document.DocumentParagraph;
 import colab.common.exception.NotApplicableException;
+import colab.common.xml.XmlNode;
+import colab.common.xml.XmlParseException;
 
 class ChangeLevel implements Applicable {
 
+    /** Serialization version number. */
+    public static final long serialVersionUID = 1L;
+
     private int headerLevel;
+
+    public ChangeLevel() {
+    }
 
     public ChangeLevel(final int headerLevel) {
         super();
@@ -21,6 +29,31 @@ class ChangeLevel implements Applicable {
 
     public int getHeaderLevel() {
         return headerLevel;
+    }
+
+    /** {@inheritDoc} */
+    public String xmlNodeName() {
+        return "ChangeLevel";
+    }
+
+    /** {@inheritDoc} */
+    public XmlNode toXml() {
+
+        XmlNode node = new XmlNode(xmlNodeName());
+        node.setContent(Integer.toString(this.headerLevel));
+        return node;
+
+    }
+
+    /** {@inheritDoc} */
+    public void fromXml(final XmlNode node) throws XmlParseException {
+
+        try {
+            this.headerLevel = Integer.parseInt(node.getBody());
+        } catch (final NumberFormatException e) {
+            throw new XmlParseException(e);
+        }
+
     }
 
 }

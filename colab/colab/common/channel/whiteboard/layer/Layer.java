191adaa6de414f8fe493878a2495bc009c08e7aa
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-04-19 23:10:44 +0000

    add LayerListener

--- colab/colab/common/channel/whiteboard/layer/Layer.java
+++ colab/colab/common/channel/whiteboard/layer/Layer.java
@@ -1,8 +1,12 @@
 package colab.common.channel.whiteboard.layer;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import colab.common.channel.whiteboard.Drawable;
+import colab.common.channel.whiteboard.draw.Figure;
 import colab.common.identity.Identifiable;
 
 /**
@@ -19,6 +23,10 @@ public class Layer implements Identifiable<LayerIdentifier>, Drawable {
      */
     private String label;
 
+    private final List<LayerListener> listeners;
+
+    private final List<Figure> figures;
+
     /**
      * Constructs a new Layer.
      *
@@ -26,6 +34,28 @@ public class Layer implements Identifiable<LayerIdentifier>, Drawable {
      */
     public Layer(final LayerIdentifier id) {
         this.id = id;
+        this.listeners = new ArrayList<LayerListener>();
+        this.figures = new LinkedList<Figure>();
+    }
+
+    public void addLayerListener(final LayerListener listener) {
+        this.listeners.add(listener);
+    }
+
+    public void removeLayerListener(final LayerListener listener) {
+        this.listeners.remove(listener);
+    }
+
+    protected void fireOnLabelChange(final String newLabel) {
+        for (LayerListener listener : listeners) {
+            listener.onLabelChange(newLabel);
+        }
+    }
+
+    protected void fireOnFigureAdded(final Figure figure) {
+        for (LayerListener listener : listeners) {
+            listener.onFigureAdded(figure);
+        }
     }
 
     /** {@inheritDoc} */
@@ -50,9 +80,18 @@ public class Layer implements Identifiable<LayerIdentifier>, Drawable {
      */
     public void setLabel(final String label) {
         this.label = label;
+        this.fireOnLabelChange(label);
+    }
+
+    public void addFigure(final Figure figure) {
+        this.figures.add(figure);
+        this.fireOnFigureAdded(figure);
     }
 
     public void draw(final Graphics graphIn) {
-        // TODO Auto-generated method stub
+        for (Figure figure : figures) {
+            figure.draw(graphIn);
+        }
     }
+
 }

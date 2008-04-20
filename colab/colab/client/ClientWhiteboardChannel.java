07a1c76b7616a92c0f2547ae5059673dc2bbccd8
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-04-20 04:43:37 +0000

    make the whiteboard channel panel look nicer, add the layer panel

--- colab/colab/client/ClientWhiteboardChannel.java
+++ colab/colab/client/ClientWhiteboardChannel.java
@@ -6,6 +6,7 @@ import colab.common.channel.ChannelData;
 import colab.common.channel.ChannelDataSet;
 import colab.common.channel.ChannelDescriptor;
 import colab.common.channel.type.WhiteboardChannelType;
+import colab.common.channel.whiteboard.Whiteboard;
 import colab.common.naming.ChannelName;
 
 public class ClientWhiteboardChannel extends ClientChannel {
@@ -13,11 +14,19 @@ public class ClientWhiteboardChannel extends ClientChannel {
     /** Serialization version number. */
     public static final long serialVersionUID = 1L;
 
+    private Whiteboard whiteboard;
+
     public ClientWhiteboardChannel(final ChannelName name)
             throws RemoteException {
 
         super(name);
 
+        whiteboard = new Whiteboard();
+
+    }
+
+    public Whiteboard getWhiteboard() {
+        return whiteboard;
     }
 
     @Override

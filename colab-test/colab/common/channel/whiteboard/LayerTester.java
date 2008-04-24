706bab267f17ef7cfd9f1658f35c9c548f19e8a4
ch.martin <ch.martin@6d828bd8-6d45-0410-8424-3f9a0b9a95ae>
2008-04-24 22:53:37 +0000

    add croppedImage test

--- colab-test/colab/common/channel/whiteboard/LayerTester.java
+++ colab-test/colab/common/channel/whiteboard/LayerTester.java
@@ -3,6 +3,7 @@ package colab.common.channel.whiteboard;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
 import junit.framework.TestCase;
 import colab.common.channel.whiteboard.draw.Ellipse;
@@ -37,6 +38,27 @@ public class LayerTester extends TestCase {
         ));
 
         assertEquals(new Rectangle(50, 50, 70, 90), layer.getBounds());
+
+    }
+
+    public void testCroppedImage() throws Exception {
+
+        Layer layer = new Layer(new LayerIdentifier(4));
+
+        layer.addFigure(new Ellipse(
+            new Point(50, 50),
+            new Dimension(25, 25)
+        ));
+
+        layer.addFigure(new Ellipse(
+            new Point(100, 100),
+            new Dimension(20, 40)
+        ));
+
+        BufferedImage croppedImage = layer.croppedImage();
+
+        assertEquals(70, croppedImage.getWidth());
+        assertEquals(90, croppedImage.getHeight());
 
     }
 
